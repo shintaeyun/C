90e16005b80001ed424671cf224aeb646fbf19bf
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-06-24 21:07:40 +0900

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,9 @@
+#incldue <stdio.h>
+
+int main() {
+int a = 100, b = 200, c = 300;
+	printf("swap 함수 전 a = %d, b = %d, c = %d\n", a, b, c);
+	swap(&a, &b, &c);
+	printf("swap 함수 후 a = %d, b = %d, c = %d\n", a, b, c);
+return 0;
+}
