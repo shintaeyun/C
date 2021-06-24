3aa991d895fda4d977c1b7a98f4e3945f2296c99
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-06-24 21:06:20 +0900

    Create Resident registration number.c

---
+++ Resident registration number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  char number[15];
+	printf("입력 : ");
+	gets(number);
+	if (number[7] == '1' || number[7] == '3')
+		printf("출력 : 남자");
+	else if (number[7] == '2' || number[7] == '4')
+		printf("출력 : 여자");
+	else
+		printf("유효하지 않음");
+  
+}
