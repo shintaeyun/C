44dc604f476480c5b36a7d030d14ef484177c8b1
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-06-26 19:45:30 +0900

    Create English order.c

---
+++ English order.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  
+  char colors[5][7] = { "Red","Green", "Yellew", "Black", "Pink" };
+	char sw[7];
+	int a;
+	for (int j = 0; j < 5; j++) {
+		for (int i = 0; i < 4; i++) {
+			a = strcmp(colors[i], colors[i + 1]);
+			if (a == 1) {
+				strcpy(sw, colors[i + 1]);
+				strcpy(colors[i + 1], colors[i]);
+				strcpy(colors[i], sw);
+			}
+		}
+	}
+	for (int i = 0; i < 5; i++)
+		printf("%s\n", colors[i]);
+  
+  return 0;
+}
