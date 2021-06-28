be77c7cdae0fea7e015ef2fd23a3add8b0878462
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-06-28 23:42:24 +0900

    Create struct reset.c

---
+++ struct reset.c
@@ -0,0 +1,15 @@
+#incldue <stdio.h>
+
+struct student {
+	int number;
+	char name[20];
+	double grade;
+};
+
+int main() {
+
+struct student s1 = { 24, "Kim", 4.3 }; 
+	printf("%d, %s, %lf", s1.number, s1.name, s1.grade);
+
+return 0;
+}
