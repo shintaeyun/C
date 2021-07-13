a226ce719d0b00176e9dcf6ad63315f884d0d513
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-07-13 18:15:11 +0900

    Update insertion sort.c

--- 7.13/insertion sort.c
+++ 7.13/insertion sort.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#define MAX 30
+int n, d[MAX];
 
+int main()
+{
+        scanf("%d", &n);
+
+
+        for (int i = 1; i <= n; i++)
+            scanf("%d", &d[i]);
+
+
+        int i = 2;
+        while (i <= n)
+        {
+            int j = i;
+            while (j >= 2 && d[j - 1] > d[j])
+            {
+                int t = d[j - 1];
+                d[j - 1] = d[j];
+                d[j] = t;
+                j = j - 1;
+            }
+            i = i + 1;
+        }
+
+
+        for (int i = 1; i <= n; i++)
+            printf("%d\n", d[i]);
+    }
+    return 0;
+}
