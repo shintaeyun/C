9a58b025e3afdf272b5cd6ba0ed63a2d1b007574
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-07-14 17:20:30 +0900

    Update Find the length of a string.c

--- 7.14/Find the length of a string.c
+++ 7.14/Find the length of a string.c
@@ -10,8 +10,9 @@ int	strLen(char *ptr) {
 
 int main() {
   
-  
-  
-  
-  
-  return 0;}
+	int len;
+	char str[50] = "this is test.";
+	printf("\n문자열 길이 : %d, %d\n", strLen(str), strlen(str));
+	
+	  return 0;
+}
