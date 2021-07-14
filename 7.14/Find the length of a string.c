3976841770bd7a1792760ce914a92de6c4409345
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-07-14 17:19:05 +0900

    Update Find the length of a string.c

--- 7.14/Find the length of a string.c
+++ 7.14/Find the length of a string.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <string.h>
 
+int	strLen(char *ptr) {
+	int len = 0;
+	while (ptr[len] != '\0')
+		len++;
+	return len;
+}
+
+int main() {
+  
+  
+  
+  
+  
+  return 0;}
