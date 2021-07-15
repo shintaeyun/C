d3b4e7777a8046618b2d3991ba3478df37172ef2
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-07-15 19:12:57 +0900

    Create star2.c

---
+++ 7.15/star2.c
@@ -0,0 +1,16 @@
+#Inlcude <stdio.h>
+
+int main() {
+  for (int i = 0; i < 5; i++) 
+	{
+		for (int j = 0; j <i; j++) 
+		{
+			printf(" ");
+			
+		}
+		for (int k =0 ; k < 5-i; k++) 
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
