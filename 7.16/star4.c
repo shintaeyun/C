47eb47e928db5a90faaef283437c8eb049a7eabf
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-07-16 23:00:55 +0900

    Create star4.c

---
+++ 7.16/star4.c
@@ -0,0 +1,17 @@
+#Include <stdio.h>
+
+int main() {
+  
+  for (int k = 0; k < 3; k++) {
+		for (int a = 1; a <= 3 - k; a++) {
+			printf("*");
+		}
+		printf("\n");
+	}	
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
