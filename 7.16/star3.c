738fb1c697f559b3022fb09a486d361097e145c0
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-07-16 22:58:27 +0900

    Update star3.c

--- 7.16/star3.c
+++ 7.16/star3.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+  
+  
+for (int i = 0; i < 3; i++) {
+		for (int j = 0; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int k = 0; k < 3; k++) {
+		for (int a = 1; a <= 3 - k; a++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
