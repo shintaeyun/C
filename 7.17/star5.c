80495b09e8748552c2733f544e74c37055176ddf
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-07-17 22:51:08 +0900

    Update star5.c

--- 7.17/star5.c
+++ 7.17/star5.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+  for (int i = 0; i < 5; i++)
+	{
+		for (int j = i; j < 5 - 1; j++)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k < i * 2 + 1; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
