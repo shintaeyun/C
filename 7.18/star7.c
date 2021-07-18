0f4e3bd57f0aaefceb3bcb9aca42ed80ab86b3c8
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-07-18 17:01:33 +0900

    Update star7.c

--- 7.18/star7.c
+++ 7.18/star7.c
@@ -1,1 +1,14 @@
+#incldue <stdio.h>
 
+int main() {
+  
+  int plus = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			printf("%-3d", ++plus);
+		}
+		printf("\n");
+	}
+}
