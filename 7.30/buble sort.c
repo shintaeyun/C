50ac706158ce67e24aced765169269ffdb6998bf
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-07-30 22:05:07 +0900

    Create buble sort.c

---
+++ 7.30/buble sort.c
@@ -0,0 +1,28 @@
+#inlcude <stdio.h>
+
+int main() {
+
+int arr[5] = { 3, 5, 1, 2, 0 };
+	int temp = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+		for (int i = 0; i < 5; i++)
+		{
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+}
