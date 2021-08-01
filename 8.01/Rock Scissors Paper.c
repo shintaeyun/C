74453396df4e24d1e89cfbcd1a22c35761f10aba
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-08-01 22:37:04 +0900

    Update Rock Scissors Paper.c

--- 8.01/Rock Scissors Paper.c
+++ 8.01/Rock Scissors Paper.c
@@ -1,1 +1,25 @@
+#include <stdioh>
+#include <stdlib.h>
+#include <time.h>
 
+int main() {
+  
+  int use, ai;
+	srand((unsigned)time(NULL));
+	printf("0.가위 1.바위 2.보\n");
+	scanf("%d", &use);
+	ai = rand() % 3;
+	printf("%d %d\n", use, ai);
+	if (ai == use)
+	{
+		printf("무승부\n");
+	}
+	else if (((use == 0) && (ai == 2)) || ((use == 1) && (ai == 0)) || ((use == 2) && (ai == 1)))
+	{
+		printf("이겼습니다.\n");
+	}
+	else
+	{
+		printf("졌습니다.\n");
+	}
+}
