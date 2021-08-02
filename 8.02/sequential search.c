84f4b4643b2561b1a23c7c5faf6539ba4dfda1d9
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-08-02 23:05:57 +0900

    Update sequential search.c

--- 8.02/sequential search.c
+++ 8.02/sequential search.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main() {
+  int arr[] = { 3, 5, 1, 7, 49, 26, 8, 11, 90, 99 };
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < sizeof(int); i++)
+	{
+		if (arr[i] == n)
+		{
+			printf("%d번 방에서 %d찾았습니다.\n", i, n);
+			break;
+		}
+		else
+		{
+			printf("찾지 못했습니다.\n");
+			break;
+
+		}
+	}
+}
