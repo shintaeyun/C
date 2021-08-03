148560827a5e28bb18c779b51c03a1e91f21f2fe
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-08-03 21:23:16 +0900

    Update binary search.c

--- 8.03/binary search.c
+++ 8.03/binary search.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main() {
+
+int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int n, left = 0, mid = 0, right = sizeof(arr)/sizeof(int) - 1;
+	scanf("%d", &n);
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+		if (n == arr[mid])
+		{
+			printf("%d방에서 %d 찾았습니다.\n", mid, n);
+			break;
+		}
+		else if (n > arr[mid])
+		{
+			left = mid + 1;
+		}
+		else if (n < arr[mid])
+		{
+			right = mid - 1;
+		}
+	}
+	if (left > right)
+		printf("값을 찾지 못했습니다.\n");
+}
