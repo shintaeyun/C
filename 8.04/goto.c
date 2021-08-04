30cad67792cf78594327ef4f5be19a2fad7acf54
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-08-04 21:45:38 +0900

    Update goto.c

--- 8.04/goto.c
+++ 8.04/goto.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+  
+  int num = 5;
+	if (num == 1) goto ONE;
+	else if (num == 2) goto TWO;
+	else goto FINSH;
+
+	ONE: printf("1\n");
+		goto FINSH;
+	TWO: printf("2\n");
+		goto FINSH;
+	FINSH: printf("FINSH\n");
+}
