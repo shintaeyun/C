501daf07f7506a15bcb8cf7af9cdb493b32ead44
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-08-06 21:50:05 +0900

    Update change to uppercase.c

--- 8.05/change to uppercase.c
+++ 8.05/change to uppercase.c
@@ -1,5 +1,20 @@
 #include <stdio.h> 
 
+void wordlist(char *str) {
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122) 
+		{
+			str[i] = str[i] - 32;
+		}
+		printf("%c", str[i]);
+		if (str[i] == ' ')
+		{
+			printf("\n");
+		}
+	}
+}	
+
 int main() {
 
   char str[30];
