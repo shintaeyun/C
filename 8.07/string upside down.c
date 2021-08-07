0c9d327597a3e44973b76b7021bb8b90f25bd39a
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-08-07 22:59:19 +0900

    Create string upside down.c

---
+++ 8.07/string upside down.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char sreverse(char *sarr, int num) {
+	for (int i = 0; i < num / 2; i++)
+	{
+		char temp = sarr[i];			//char temp = *(sarr + i);
+		sarr[i] = sarr[num - 1 - i];	//*(sarr + i) = *(sarr + num - i - 1);
+		sarr[num - 1 - i] = temp;		//*(sarr + num - i - 1) = temp;
+	}
+}
+
+int main() {
+  
+  char* sarr;
+	sarr = (char*)malloc(100);
+	strcpy(sarr, "this is example. ");
+	printf("\n\n%s\n", sarr);
+	sreverse(sarr, strlen(sarr));
+	printf("%s\n", sarr);
+	free(sarr);
+  
+}
