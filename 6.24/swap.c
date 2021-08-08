d4b34cb0b1e3ef93cf5462a5a699cbce656ca38a
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-08-08 22:39:03 +0900

    Update swap.c

--- 6.24/swap.c
+++ 6.24/swap.c
@@ -1,5 +1,15 @@
 #incldue <stdio.h>
 
+void swap(int* a, int* b, int* c)
+{
+	int temp = 0;
+	temp = *a;
+	*a = *b;
+	*b = *c;
+	*c = temp;
+}
+
+
 int main() {
 int a = 100, b = 200, c = 300;
 	printf("swap 함수 전 a = %d, b = %d, c = %d\n", a, b, c);
